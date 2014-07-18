d000fbc20f74339ffbbdcaa1281cd8bac2654b7e
Rory O’Kane <rory@roryokane.com>
2014-07-18 01:00:57 -0400

    Add shoulda-matchers and make current tests use them

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ group :test do
   gem 'faker'
   gem 'launchy'
   gem 'selenium-webdriver'
+  gem 'shoulda-matchers'
 end
 group :production do
   gem 'rails_12factor'
--- spec/models/podcast_spec.rb
+++ spec/models/podcast_spec.rb
@@ -1,15 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Podcast, :type => :model do
-  it "rejects podcasts with no title" do
-    podcast = create(:podcast)
-    podcast.title = ""
-    expect(podcast.save).to be false
-  end
-
-  it "rejects podcasts with no description" do
-    podcast = create(:podcast)
-    podcast.description = ""
-    expect(podcast.save).to be false
-  end
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:description) }
 end
