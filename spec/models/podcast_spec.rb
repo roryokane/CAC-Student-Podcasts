e9c1dbe833326da5028cdc37dfa3ac73c7a57248
Rory O’Kane <rory@roryokane.com>
2014-07-17 21:56:11 -0400

    Write Podcast model tests
    I don’t need an explicit test for accepting podcasts with a correct title and description, since the factory test does that

--- spec/models/podcast_spec.rb
+++ spec/models/podcast_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Podcast, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "rejects podcasts with no title" do
+    podcast = create(:podcast)
+    podcast.title = ""
+    expect(podcast.save).to be false
+  end
+
+  it "rejects podcasts with no description" do
+    podcast = create(:podcast)
+    podcast.description = ""
+    expect(podcast.save).to be false
+  end
 end
