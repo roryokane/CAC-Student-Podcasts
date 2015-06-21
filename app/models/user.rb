1d776f1b5fe04322683ca1377167ff28d33e170a
Rory O’Kane <rory@roryokane.com>
2015-06-21 08:32:32 -0400

    Add role column to users
    Using the example code in http://railsapps.github.io/rails-authorization.html, section “Examples” > “User Model”

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ActiveRecord::Base
-  # The other Devise modules available are:
-  # :registerable, :confirmable, :lockable, :timeoutable and :omniauthable
+  enum role: [:user, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :user
+  end
+
   devise :database_authenticatable, :recoverable, :rememberable,
          :trackable, :validatable
+  # The other Devise modules available are:
+  # :registerable, :confirmable, :lockable, :timeoutable and :omniauthable
 end
